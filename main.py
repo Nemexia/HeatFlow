ab29a937660eeb661fd8ff2306d55ba3c245d9a3
Nemexia <amirhossein.rahmani55@gmail.com>
2024-02-22 21:55:33 +0330

    fix: move step fucntion to Model class

--- main.py
+++ main.py
@@ -5,32 +5,13 @@ import models
 import plotter
 
 
-def step(space, mode):
-    next_space = np.copy(space)
-    for x, r in enumerate(space):
-        for y, t in enumerate(r):
-            if mode[x, y] == 0:  # if conductor
-                total = 0
-                count = 1
-                for i in range(x - 1, x + 2):
-                    for j in range(y - 1, y + 2):
-                        if mode[i, j] != -1:  # if not insulator
-                            total += space[i, j]
-                            count += 1
-                next_space[x, y] = total / count
-    return next_space
-
-
 model = models.Model()
-temp = model.temperature
-mode = model.mode
 plot = plotter.plot
 
 
-plt.imshow(mode, cmap='binary')
+plt.imshow(model.mode, cmap='binary')
 plt.show()
 
 while True:
-    plot(temp)
-    temp = step(temp, mode)
-    # input()
+    plot(model.temperature)
+    model.step()
--- models.py
+++ models.py
@@ -9,7 +9,6 @@ class Model:
         self.temperature[0, :] = 1000
         self.temperature[self.space_size[0] - 1, :] = 0
         
-        
         # mode definition
         # 0:conductor  -1:insulator  1:constant
         self.mode = np.zeros(self.space_size)
@@ -19,15 +18,19 @@ class Model:
 
         self.mode[1:-1, 0] = -1
         self.mode[1:-1, self.space_size[1] - 1] = -1
-
-
-
-
-
-# for x, r in enumerate(material):
-#     for y, t in enumerate(r):
-#         material[x, y] = 25
-
-
-
-
+    
+    def step(self):
+        next_temp = np.copy(self.temperature)
+        for x, r in enumerate(self.temperature):
+            for y, t in enumerate(r):
+                if self.mode[x, y] == 0:  # if conductor
+                    total = 0
+                    count = 1
+                    for i in range(x - 1, x + 2):
+                        for j in range(y - 1, y + 2):
+                            if self.mode[i, j] != -1:  # if not insulator
+                                total += self.temperature[i, j]
+                                count += 1
+                    next_temp[x, y] = total / count
+        self.temperature = next_temp
+        
